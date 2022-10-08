98ffe85d0d42302ead663fe3de2e7d0267e9b04d
megadubcev2 <91036607+megadubcev2@users.noreply.github.com>
2022-10-08 23:11:58 +0300

    Add files via upload

---
+++ aestarodubtsevPW2/aestarodubtsevPW2/SceneDelegate.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    func scene(_ scene: UIScene, willConnectTo session:
+    UISceneSession, options connectionOptions:
+    UIScene.ConnectionOptions) {
+        
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+    let window = UIWindow(windowScene: windowScene)
+    let navigationController = UINavigationController(rootViewController: WelcomeViewController())
+        window.rootViewController = navigationController
+        //window.rootViewController = ViewController()
+
+        self.window = window
+    window.makeKeyAndVisible()
+    }
+    
+
+    
+}
